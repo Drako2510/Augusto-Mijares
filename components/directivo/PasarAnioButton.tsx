750fabf1c4bbb1077d1c1e6134bf570a0fdb8302
Drako2510 <jesusdaviddacosta@gmail.com>
2026-07-26 02:59:29 -0400

    Augusto Mijares

--- components/directivo/PasarAnioButton.tsx
+++ components/directivo/PasarAnioButton.tsx
@@ -151,7 +151,7 @@ export function PasarAnioButton() {
                       <select value={seccion} onChange={(e) => setSeccion(e.target.value)}
                         className="w-full rounded-lg border border-gray-300 dark:border-gray-600 bg-white dark:bg-gray-800 px-3 py-2 text-sm text-gray-700 dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-amber-500">
                         <option value="">Seleccionar sección...</option>
-                        {secciones.map((s) => <option key={s} value={s}>"{s}"</option>)}
+                        {secciones.map((s) => <option key={s} value={s}>&quot;{s}&quot;</option>)}
                       </select>
                     </div>
                   </div>
